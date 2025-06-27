271a5cc31607cc68aa3aadb7e05117b647bd12f7
saifAashiq71 <saifAashiq71@gmail.com>
2025-06-28 03:18:46 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -36,19 +36,28 @@ fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
+  # Version & Variant
+   export FOX_VERSION="R11.1"
+   export FOX_VARIANT="A14"
+   export FOX_BUILD_TYPE="Stable"
+# OrangeFox Addons
+   export FOX ENABLE_APP_MANAGER-1
 
-
-
-
-
-
-lunch twrp_$FDEVICE-eng
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- 	fi
+#Binaries & Tools
+export FOX_USE_BASH_SHELL-1
+export FOX ASH_IS BASH-1
+export FOX_USE_NANO_EDITOR-1
+export FOX_USE_TAR_BINARY-1
+export FOX USE_SED_BINARY-1
+export FOX_USE_XZ_UTILS 1
+   
+          lunch twrp_$FDEVICE-eng
+	  # let's see what are our build VARs
+	  if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	     export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	     export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+	     export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	     export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	  fi
 fi
 #
